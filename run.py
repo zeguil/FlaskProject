66667fa77fa29b2926bdd367b824167dc4c956b0
José Guilherme <61730073+zeguil@users.noreply.github.com>
2021-05-17 10:40:08 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 
-
+#beekeeper
+#sqlite browser
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
